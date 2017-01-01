be280a8dc14e190468499351d051de28adf17ec4
Jagapathi126 <jagapathi.126@gmail.com>
2017-01-01 15:24:10 +0530

    Fixing issue with multiple notifcations from single user and updating user docs if contact is not shared

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,10 @@
 // Service to return users or create user or delete user
 var UserSchema = require("./../models/user");
 var StringUtils = require("./../common/StringUtils");
-var constants = require("./../common/constants")
+var constants = require("./../common/constants");
+var NotificationSchema = require("./../models/notification");
+var _ = require("lodash");
+var Q = require("q");
 var userService = {
     createUser: function (doc) {
         var userDoc = new UserSchema.UserModel();
@@ -28,6 +31,39 @@ var userService = {
             }
             return false;
         });
+    },
+    updateConnectedUser: function (users, uid) {
+        var d = Q.defer();
+
+        _.remove(users, function (user) {
+            return user[constants.userDocKeys._id] == uid;
+        });
+        var searchObj = {};
+        searchObj[constants.notificationKeys.requestor] = uid;
+
+        NotificationSchema.NotificationModel.find(searchObj).exec()
+            .then(function (notifications) {
+                users.forEach(function (user, i) {
+                    var index = _.findIndex(notifications, function (notification) {
+                        return notification[constants.notificationKeys.responder] == users[i][constants.userDocKeys._id]
+                    });
+                    if (index == -1) {
+                        users[i][constants.userDocKeys.phone] = "";
+                        users[i][constants.userDocKeys.email] = "";
+                    } else if (notifications[index][constants.notificationKeys.isInProgress]) {
+                        users[i][constants.userDocKeys.phone] = "";
+                        users[i][constants.userDocKeys.email] = "";
+                    } else if (!(notifications[index][constants.notificationKeys.status])) {
+                        users[i][constants.userDocKeys.phone] = "";
+                        users[i][constants.userDocKeys.email] = "";
+                    }
+                });
+                d.resolve(users);
+            }).catch(function (err) {
+                d.reject(err);
+            });
+
+        return d.promise;
     }
 }
 
